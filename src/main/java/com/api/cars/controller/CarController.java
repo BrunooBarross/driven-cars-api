adef1b7ebce364ced921bdcd32a4c38fea7c83ca
Bruno_Barros <barrosbrunofelipe@gmail.com>
2023-01-19 20:15:31 -0300

    feat: adicionando rota para deletar um carro

--- src/main/java/com/api/cars/controller/CarController.java
+++ src/main/java/com/api/cars/controller/CarController.java
@@ -6,12 +6,15 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,6 +46,15 @@ public class CarController {
     @GetMapping
     public List<Car> listAll() {
         return repository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCar(@PathVariable Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+           e.printStackTrace();
+        }
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
